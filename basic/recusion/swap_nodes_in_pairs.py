70fde83a241f28e090ee929ce0699fdc5b92757f
Chris Lin <chris4540@gmail.com>
2020-08-30 10:55:30 +0200

    Bug fix edge case

--- basic/recusion/swap_nodes_in_pairs.py
+++ basic/recusion/swap_nodes_in_pairs.py
@@ -2,31 +2,38 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-    
+
     def print(self):
         n = self
         while n:
             print(n.val, end="")
-            print("->", end="")
             n = n.next
+            if n:
+                print("->", end="")
         print("")
+
 
 def swap_nodes(node: ListNode) -> ListNode:
     # swap this node and the next node
-    if node is None or node.next is None:
+
+    # terminal case 1: we go to the end
+    if node is None:
         return None
+
+    # terminal case 2: we don't have next, cancel swap
+    if node.next is None:
+        return node
 
     # -------------------------
     # swap node and the next ndoe
     first = node.next
     second = node
-    second.next = swap_nodes(first.next)  #  swap the children first 
+    second.next = swap_nodes(first.next)  # swap the children first
     first.next = second  # implement swap
     return first
-    
-if __name__ == "__main__":
-    arr = [1, 2, 3, 4]
 
+
+def test_arr(arr):
     # build link list
     head = ListNode(arr[0])
     prev = head
@@ -34,7 +41,12 @@ if __name__ == "__main__":
         node = ListNode(x)
         prev.next = node
         prev = node
-    
-    head.print()
-    head2 = swap_nodes(head)
-    head2.print()
+
+    # head.print()
+    swap_nodes(head).print()
+
+if __name__ == "__main__":
+    test_arr([1, 2, 3, 4])
+    test_arr([1, 2, 3, 4, 5])
+
+
