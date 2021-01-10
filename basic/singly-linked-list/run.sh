5606c51a3536132960137dd7a5d1af31cef25d98
Chris Lin <chris4540@gmail.com>
2021-01-10 01:03:59 +0100

    Add run script

--- basic/singly-linked-list/run.sh
+++ basic/singly-linked-list/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-g++ ./singly-linked-list.cc -fsanitize=address -g3 -O1 -fno-omit-frame-pointer -o ./singly-linked-list
+g++ ./singly-linked-list.cc -fsanitize=address -g3 -O1 -fno-omit-frame-pointer -o ./singly-linked-list.x
 
-./singly-linked-list
+./singly-linked-list.x
