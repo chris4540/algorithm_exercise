43b6629e1397393d94de7fb19856f1e68a528c31
Chris Lin <chris4540@gmail.com>
2021-03-18 23:22:12 +0800

    Implement a simple approach of topo sort

--- leetcode/graphs/course_schedule_2/topo_sort.py
+++ leetcode/graphs/course_schedule_2/topo_sort.py
@@ -7,47 +7,51 @@ from typing import List
 
 class Solution:
 
-
-    def _dfs(self, v, adj_list, visited, stack):
-
-        # first mark the list to be true
-        visited[v] = True
-
-        for nei in adj_list[v]:
-            if not visited[nei]:
-                self._dfs(nei, adj_list, visited, stack)
-
-        stack.append(v)
-
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
 
-
-        # here we use the adjcenty list
+        # here we use an adjcenty list to represent a graph
         adj_list = {k: [] for k in range(numCourses)}
 
+        # and a map to save down the in-degree of each vertex
+        indegree = {k: 0 for k in range(numCourses)}
 
-        # build the graph
+        # build the graph and track the indegree of each vertex
+        # an edge: (u, v) means the course `u` depends on the course `v`
+        #  i.e.     u ----> v
         for u, v in prerequisites:
-            adj_list[v].append(u)
+            adj_list[u].append(v)
+            indegree[v] += 1
 
-        # ----------------------------------
-        # Topological sorting with dfs
-        # ----------------------------------
-        visited = [False] * numCourses
+        # the sorted list
+        topo_sorted = []
 
-        # The travaling history
-        stack = []
+        # capture all nodes of indegree 0
+        while indegree:
+            cand = None
+            for node, d in indegree.items():
+                if d == 0:
+                    cand = node
+                    break
+            # the indegree dictionary is not empty and candidate is None
+            #   => We have a loop in the graph
+            if cand is None and indegree:
+                return []
 
-        # loop over all vertices to make sure no vertex missed
-        for i in range(numCourses):
-            if not visited[i]:
-                self._dfs(i, adj_list, visited, stack)
+            assert cand is not None
 
-        ret = stack[::-1]
-        return ret
+            # mark the candidate as shallow and "remove" the candidate from the graph
+            indegree.pop(cand)
+            topo_sorted.append(cand)
+
+            # loop over acessible neigborhoods
+            for nei in adj_list[cand]:
+                indegree[nei] -= 1
+
+        return topo_sorted[::-1]
+
 
 
 if __name__ == '__main__':
     sol = Solution()
-    # sol.findOrder(4, [[1,0],[2,0],[3,1],[3,2]])
+    # print(sol.findOrder(4, [[1,0],[2,0],[3,1],[3,2]]))
     print(sol.findOrder(2, [[0, 1], [1, 0]]))
